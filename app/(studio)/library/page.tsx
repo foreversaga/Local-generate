f33cfa0cadfbb8a429c580c250b7dc7b638877bf
Barry Chiu <53308160+foreversaga@users.noreply.github.com>
2026-08-18 00:08:41 +0800

    use compact library page header

--- app/(studio)/library/page.tsx
+++ app/(studio)/library/page.tsx
@@ -1,3 +1,6 @@
 import { LibraryWorkspace } from "../../components/library/LibraryWorkspace";
 import { RoutePage } from "../../components/shell/RoutePage";
-export default function LibraryPage(){return <RoutePage eyebrow="page.library.eyebrow" title="page.library.title" description="page.library.description"><LibraryWorkspace/></RoutePage>}
+
+export default function LibraryPage() {
+  return <RoutePage compact eyebrow="page.library.eyebrow" title="page.library.title" description="page.library.description"><LibraryWorkspace /></RoutePage>;
+}
