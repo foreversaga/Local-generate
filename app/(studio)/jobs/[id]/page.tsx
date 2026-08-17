dcad4be61f069a0b3df4d2ba6649cc69599e9cf5
Barry Chiu <53308160+foreversaga@users.noreply.github.com>
2026-08-18 00:08:49 +0800

    use compact job detail header

--- app/(studio)/jobs/[id]/page.tsx
+++ app/(studio)/jobs/[id]/page.tsx
@@ -1,5 +1,10 @@
 import { JobDetailWorkspace } from "../../../components/jobs/JobDetailWorkspace";
 import { RoutePage } from "../../../components/shell/RoutePage";
 
-type Props={params:Promise<{id:string}>;searchParams:Promise<{source?:string}>};
-export default async function JobDetailPage({params,searchParams}:Props){const {id}=await params;const {source}=await searchParams;return <RoutePage eyebrow="page.jobDetail.eyebrow" title="page.jobDetail.title" titleVariables={{id}} description="page.jobDetail.description"><JobDetailWorkspace jobId={id} sourceHint={source}/></RoutePage>}
+type Props = { params: Promise<{ id: string }>; searchParams: Promise<{ source?: string }> };
+
+export default async function JobDetailPage({ params, searchParams }: Props) {
+  const { id } = await params;
+  const { source } = await searchParams;
+  return <RoutePage compact eyebrow="page.jobDetail.eyebrow" title="page.jobDetail.title" titleVariables={{ id }} description="page.jobDetail.description"><JobDetailWorkspace jobId={id} sourceHint={source} /></RoutePage>;
+}
